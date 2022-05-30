f748504b8054a6a2757dc75c9bd167eda053e00b
lightcow <joonhyoung10@gmail.com>
2022-05-30 21:04:46 +0900

    test

---
+++ hook-lambda-iam.tf
@@ -0,0 +1,115 @@
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = aws_iam_role.role_for_LDC.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+            Sid = "",
+            Effect = "Allow",
+            Action = "dynamodb:*",
+            Resource = "*"
+        }
+     ]
+    })
+}
+
+resource "aws_iam_role" "role_for_LDC" {
+  name = "dynamodbrule"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+         Action = "sts:AssumeRole"
+         Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+         Effect = "Allow"
+         Sid = ""
+       }
+     ]
+    })
+}
+
+
+resource "aws_iam_role" "status" {
+  name = "lambda_role_status"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "status" {
+  role       = aws_iam_role.status.name
+  policy_arn = aws_iam_policy.status.arn
+}
+
+resource "aws_iam_policy" "status" {
+    policy = data.aws_iam_policy_document.status.json
+}
+
+data "aws_iam_policy_document" "status" {
+
+  statement {
+    sid       = "AllowInvokingLambdas"
+    effect    = "Allow"
+    resources = ["arn:aws:lambda:ap-northeast-2:*:function:*"]
+    actions   = ["lambda:InvokeFunction"]
+  }  
+
+
+  statement {
+    sid       = ""
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = [
+        "dynamodb:BatchGetItem",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem"
+        ]
+  }
+
+  statement {
+    sid       = "AmazonAPIGatewayInvokeFullAccess"
+    effect    = "Allow"
+    resources = ["arn:aws:execute-api:*:*:*"]
+    actions   = [
+      "execute-api:Invoke",
+      "execute-api:ManageConnections"
+      ]
+  }  
+
+  statement {
+    sid       = "AllowCreatingLogGroups"
+    effect    = "Allow"
+    resources = ["arn:aws:logs:ap-northeast-2:*:*"]
+    actions   = ["logs:CreateLogGroup"]
+  }
+  statement {
+    sid       = "AllowWritingLogs"
+    effect    = "Allow"
+    resources = ["arn:aws:logs:ap-northeast-2:*:log-group:/aws/lambda/*:*"]
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+  }
+}
+
---
+++ status-lambda.tf
@@ -0,0 +1,24 @@
+
+resource "aws_lambda_function" "terrform-status-lambda" {
+  # If the file is not in the current working directory you will need to include a 
+  # path.module in the filename.
+  filename      = "${data.archive_file.zip.output_path}"
+  function_name = "terrform-status-lambda"
+  role          = aws_iam_role.status.arn
+  handler       = "status_handler.handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
+  runtime = "nodejs16.x"
+
+
+}
+
+ data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambdadb/status_handler.js"
+  output_path = "${path.module}/lambdadb/status.handler.zip"
+}
