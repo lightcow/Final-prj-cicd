c821238ad332c06ac0fb9e14e3d756958c17a9ab
lightcow <joonhyoung10@gmail.com>
2022-05-31 11:40:14 +0900

    test

--- main.tf
+++ main.tf
@@ -3,18 +3,15 @@ terraform {
     aws = {       
       source  = "hashicorp/aws"       
       version = "4.12.1"    
-       }   
-      }
-       backend "remote" {
-        organization = "lightcow"
-
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "cicdtest"
+      }   
+    }
+    
+    cloud "remote" {
+      organization = "lightcow"
+      workspaces {
+        name = "cicdtest"
         }
-        
       }
-   
     }
 
 provider "aws" {
